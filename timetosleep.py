5da26f19e0c97eb66981385cef5b0299ecc70c15
Tom Nijhuis <tomnijhuis@gmail.com>
2018-04-24 17:04:35 +0200

    Include midnight setting

--- timetosleep.py
+++ timetosleep.py
@@ -2,17 +2,17 @@
 
 import time
 import sys
-from datetime import datetime, timedelta
+import datetime
 import pytz
 from dateutil import parser
 import requests
 
 # Cmd args:
 #    offset in minutes (pos or neg intger)
-#    event 'sunset', 'sunrise'
+#    event 'sunset', 'sunrise', 'midnight'
 
 ## Constants ##
-TIMEZONE_AMS = pytz.timezone('Europe/Amsterdam')
+ams = pytz.timezone('Europe/Amsterdam')
 lat, lon = '52.070498', '4.300700'
 api_url = 'https://api.sunrise-sunset.org/json?lat={}&lng={}&formatted=0'.format(lat, lon)
 
@@ -21,13 +21,17 @@ offset = 0 if len(sys.argv) < 2 else int(sys.argv[1])
 event = 'sunset' if len(sys.argv) < 3 else sys.argv[2]
 
 ## API call to json ##
-json_data = requests.get(url=api_url).json()
+if event in ['sunset', 'sunrise']:
+  json_data = requests.get(url=api_url).json()
 # print([s for s in json_data['results']])
 
 ## Time difference ##
-cur_time = datetime.now(tz=TIMEZONE_AMS)
-sns_time = parser.parse(json_data['results'][event])
-unt_time = sns_time + timedelta(minutes=offset)
+cur_time = ams.localize(datetime.datetime.now())
+if event == 'midnight':
+  sns_time = ams.localize(datetime.datetime(cur_time.year, cur_time.month, cur_time.day, 23, 59, 59))
+else:
+  sns_time = parser.parse(json_data['results'][event])
+unt_time = sns_time + datetime.timedelta(minutes=offset)
 dif_time = unt_time-cur_time
 
 # print status
